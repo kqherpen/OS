183b2afffb253190ea08bf800bfeb5809f3c159c
Kylian van Herpen <kq.vanherpen@avans.nl>
2023-11-09 15:51:11 +0100

    click func

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 const Buttons = () => {
+const[clicks, setClicks] = useState(0);
+
+function handleClick() {
+  setClicks(clicks + 1);
+}
+function resetClicks() {
+  setClicks(0)
+}
   return (
     <div className="buttons-container">
-      <button className="add-btn" type="button">
-        Add to Cart (0)
+      <button onClick={handleClick} className="add-btn" type="button">
+        Add to Cart ({clicks})
       </button>
 
-      <button className="cancel-btn" type="button">
+      <button onClick={resetClicks} className="cancel-btn" type="button">
         Cancel Order
       </button>
     </div>
